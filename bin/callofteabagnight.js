c83f7d5edcc9457e0e190bc225c1503bf40d6d4d
DavidCoenFish <fish_dsc@yahoo.co.uk>
2020-03-12 00:46:49 +1100

    cleanup, version

--- bin/callofteabagnight.js
+++ bin/callofteabagnight.js
@@ -3,6 +3,37 @@ const DriveJson = require("./../index.js");
 
 console.log("start");
 
+
+function formatDate() {
+	var date = new Date();
+	var year = date.getFullYear();
+	var month = date.getMonth() + 1; // months are zero indexed
+	var day = date.getDate();
+	var hour = date.getHours();
+	var minute = date.getMinutes();
+	var second = date.getSeconds();
+
+	if (month < 10){
+		month = "0" + month;
+	}
+
+	if (day < 10){
+		day = "0" + day;
+	}
+
+	if (hour < 10){
+		hour = "0" + hour;
+	}
+	if (minute < 10){
+		minute = "0" + minute;
+	}
+	if (second < 10){
+		second = "0" + second;
+	}
+
+	return "" + year + month + day + hour + minute + second;
+}
+
 var dataServer = undefined
 var baseObject = {}
 //make the gamedata.json
@@ -14,7 +45,16 @@ Q(DriveJson.DataServer.factoryPromise()).then(function(in_dataServer){
 	var cursor = DriveJson.Cursor();
 	return DriveJson.SheetToObject.sheet5thToObject(dataServer, in_driveCursor, cursor, baseObject);
 }).then(function(){
-	return DriveJson.Util.writeFilePromise("gamedata.json", JSON.stringify(baseObject, null, '\t'));
+	var version = "";
+	if ("version" in baseObject)
+	{
+		version = baseObject["version"];
+	}
+	version += "." + formatDate();
+
+	baseObject["version"] = version;
+
+	return DriveJson.Util.writeFilePromise("output/gamedata.json", JSON.stringify(baseObject, null, '\t'));
 }).fail(function(error){
 	console.log("error:" + error);
 	process.exit(1); //error
